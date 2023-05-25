ca1b817f3a30e33511ee9ed16e531d0a014bae10
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-05-25 20:19:03 +0900

    k번째 약수 구하기

--- cote.py
+++ cote.py
@@ -1,1 +1,10 @@
-print("hello")
+n,k = map(int,input().split())
+nList = []
+for i in range(1,n):
+    if n % i == 0 :
+        nList.append(i)
+
+if len(nList) < k :
+    print("-1")
+else:
+    print(nList[k-1])
