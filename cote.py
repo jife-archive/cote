5a50a7a478a8720871d6e6ed2ac247424a2149e7
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-24 15:32:30 +0900

    두 리스트 합치기

--- cote.py
+++ cote.py
@@ -1,19 +1,34 @@
-#카드 역배치
+#두 리스트 합치기
+import random
 
-List = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-k = list(map(int, input().split()))
-if k[1] < k [0]:
-    tmp = k[0]
-    k[0] = k[1]
-    k[1] = tmp
+a = int(input())
+b =int(input())
 
-n= k[1] - k[0]
+alist = random.sample(range(1,100),a)
+blist = random.sample(range(1,100),b)
+alist.sort()
+blist.sort()
 
-for i in range(n//2):
-    List[(k[0]-1)+i],List[(k[1]-1)-i] =List[(k[1]-1)-i],List[(k[0]-1)+i]
+#sumlist = alist + blist
+#sumlist.sort()  시간복잡도 때문에 다른거 사용
 
-print(List)
+p1 = 0
+p2 = 0 
+sumlist = []
 
+while p1< len(alist) and p2<len(blist):
+    if alist[p1] <= blist[p2]:
+        sumlist.append(alist[p1])
+        p1 += 1
+    else:
+        sumlist.append(blist[p2])
+        p2 += 1
+if p1< a:
+    sumlist = sumlist + alist[p1:]
+if p2 <b:
+    sumlist = sumlist + blist[p2:]
+
+print(sumlist)
 
 
    
