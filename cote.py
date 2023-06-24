ab3e19f68ff9232114f788e51306304b0bf92bbb
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-24 16:59:45 +0900

    수정렬 2750문제

--- cote.py
+++ cote.py
@@ -1,32 +1,12 @@
 #수 들의 합
-import random
 
+import random
 n = int(input())
-c = int(input())
 nlist = []
+for i in range(n):
+   nlist.append(int(input()))
+
+nlist.sort()
 
 for i in range(n):
-    nlist.append(random.randint(1,c))
-
-lt = 0
-rt = 1
-cnt = 0
-tot = nlist[0]
-while 1:
-    if tot < c:
-        if rt < n:
-            tot += nlist[rt]
-            rt += 1
-        else:
-            break
-    elif tot == c:
-        cnt += 1
-        tot -= nlist[lt]
-        lt += 1
-    else:
-        tot -= nlist[lt]
-        lt += 1
-
-
-print(nlist)
-print(cnt)
+    print(nlist[i])
