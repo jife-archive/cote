1c00757d103d1a5978785c89e4e9bff23a8356bd
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-25 14:12:47 +0900

    격자판최대합

--- cote.py
+++ cote.py
@@ -3,13 +3,26 @@ import sys
 input = sys.stdin.readline
 N = int(input())
 
-Nlist = []
-if N < 1 and N >1000000:
-    print("error")
-else:
+Nlist = [0 for _ in range(N)]
+rowsum = []
+colsum = []
+carsum = [0,0]
+if 1<=N and N<=50:
     for i in range(N):
-        Nlist.append(int(input()))
-    for i in sorted(Nlist):
-        print(i)
-                    
-# sys.stdin.readline이 input()보다 빠르단것을 알게되었다. 흑흑 ㅠㅠ
+        Nlist[i] = list(map(int,input().split()))
+
+    rowsum = [sum(row)for row in Nlist]
+    colsum = [sum(col)for col in zip(*Nlist)]
+    for i in range(N):
+        carsum[0] += Nlist[i][i]
+        carsum[1] += Nlist[i][-1-i]
+    rowsum.sort()
+    colsum.sort()
+    carsum.sort()
+    totalsum = []
+    totalsum.append(rowsum[-1])
+    totalsum.append(colsum[-1])
+    totalsum.append(carsum[-1])
+    print(max(totalsum))
+else:
+    print("error")
