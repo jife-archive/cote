75e617bfac61c2ab2d58c4fbdf884470b002baca
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-26 16:35:07 +0900

    소트인사이드 1427번문제

--- cote.py
+++ cote.py
@@ -1,27 +1,17 @@
 
 import sys
-import random
 
+Nlist = []
 N = int(sys.stdin.readline())
-sum = 0
-if N<= 20 and N>=3 and N%2==1:
-    Nlist = [[] for _ in range(N)]
-    for i in range(N):
-            Nlist[i] = list(map(int, sys.stdin.readline().split())) ##중요 한줄로 입력받기!
-    s=e=N//2
-    for i in range(N):
-         print(i)    
-         for j in range(s, e+1):
-            sum += Nlist[i][j]   
-         if i < N//2:
-            s -=1
-            e +=1
-         else:
-              s+=1
-              e-=1          
-else:
-    print("Error")
-print(sum)
+
+while (N>0):
+    Nlist = Nlist + [N%10]
+    N //= 10
+
+Nlist.sort(reverse=True)
+Nlist = list(map(str, Nlist))
+a = "".join(Nlist)
+print(a)
 
 
 
