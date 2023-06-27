743f550f6f41bcc057db3c8e35c619c2cbd07a46
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-27 15:30:52 +0900

    곳감(모래시계)

--- cote.py
+++ cote.py
@@ -2,14 +2,36 @@
 import sys
 
 N = int(sys.stdin.readline())
-result = []
+sum = 0
+if N>=3 and N<=20 and N%2==1:
+    Nlist = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+    M = int(sys.stdin.readline())
+    if M>=1 and M<=10:
+        Mlist = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+        for i in range(M):
+            row = Mlist[i][0]
+            direction = Mlist[i][1]
+            cnt = Mlist[i][2]
+            for j in range(cnt):
+                        if row <= N and direction == 0:
+                            Nlist[row].append(Nlist[row].pop(0)) ##0번 인덱스의 자료를 팝하고 뒤에거 댕김
+                        elif row <= N and direction == 1:
+                             Nlist[row].insert(0,Nlist[row].pop())##inset는 0번자리에 값을 넣어라 -> pop()젤 뒤의 데이터를 갖고온다            
+else:
+    print("error")
 
-Nlist = [sys.stdin.readline().strip() for _ in range(N)] 
-
-
-result = list(set(Nlist))
-result.sort()
-result.sort(key=lambda x: len(x))
-
-for i in range(len(result)):
-    print(result[i])
+q = 0
+r = N-1
+print(Nlist)
+for i in range(N):
+    print("i:",i)
+    for j in range(q,r+1):
+        print(Nlist[i][j])
+        sum += Nlist[i][j]
+    if i < N//2:
+        q+=1
+        r-=1
+    else:
+        q-=1
+        r+=1    
+print("총합:",sum)
