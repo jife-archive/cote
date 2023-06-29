ed272f8a9f88ee9c3e9aefedebf561e6636299e6
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-29 17:14:19 +0900

    봉우리문제

--- cote.py
+++ cote.py
@@ -1,37 +1,71 @@
-
 import sys
 
 N = int(sys.stdin.readline())
-sum = 0
-if N>=3 and N<=20 and N%2==1:
-    Nlist = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-    M = int(sys.stdin.readline())
-    if M>=1 and M<=10:
-        Mlist = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
-        for i in range(M):
-            row = Mlist[i][0]
-            direction = Mlist[i][1]
-            cnt = Mlist[i][2]
-            for j in range(cnt):
-                        if row <= N and direction == 0:
-                            Nlist[row].append(Nlist[row].pop(0)) ##0번 인덱스의 자료를 팝하고 뒤에거 댕김
-                        elif row <= N and direction == 1:
-                             Nlist[row].insert(0,Nlist[row].pop())##inset는 0번자리에 값을 넣어라 -> pop()젤 뒤의 데이터를 갖고온다            
-else:
+Nlist = [[]for _ in range(N)]
+topcount = 0
+if N < 1 and N >50:
     print("error")
+else:
+    for i in range(N):
+        Nlist[i] += list((sys.stdin.readline().split()))
 
-q = 0
-r = N-1
-print(Nlist)
+t = b = l = r = 0
+
 for i in range(N):
-    print("i:",i)
-    for j in range(q,r+1):
-        print(Nlist[i][j])
-        sum += Nlist[i][j]
-    if i < N//2:
-        q+=1
-        r-=1
+    if i == 0 or i == N-1:
+        if i == 0:
+            t = 0
+            b = i+1
+        elif i == N-1:
+            t = i-1
+            b = 0
     else:
-        q-=1
-        r+=1    
-print("총합:",sum)
+        t = i -1
+        b = i+1
+    for j in range(N):
+        if j == N-1:
+            l = j-1
+            if t == 0:
+                if Nlist[i][j] > Nlist[i][l] and Nlist[i][j] > Nlist[b][j]:
+                    topcount +=1
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+            elif b == 0:
+                if Nlist[i][j] > Nlist[t][j] and Nlist[i][j] > Nlist[i][l]:
+                    topcount += 1
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+            elif t > 0 and b > 0:
+                if Nlist[i][j] > Nlist[i][l] and Nlist[i][j] > Nlist[b][j] and Nlist[i][j] > Nlist[t][j]:
+                    topcount +=1
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+        if j == 0:
+            r = j+1
+            if t == 0:
+                if Nlist[i][j] > Nlist[i][r] and Nlist[i][j] > Nlist[b][j]:
+                    topcount +=1
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+            elif b == 0:
+                if Nlist[i][j] > Nlist[i][r] and Nlist[i][j] > Nlist[t][j]:
+                    topcount +=1
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+            elif t > 0 and b >0:
+                if Nlist[i][j] > Nlist[i][l] and Nlist[i][j] > Nlist[b][j] and Nlist[i][j] > Nlist[t][j]and Nlist[i][j]>Nlist[i][r]:
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+                    topcount +=1
+                
+        if j > 0 and j < N -1:
+            r = j+1
+            l = j-1
+            if t == 0:
+                if Nlist[i][j] > Nlist[i][r] and Nlist[i][j] > Nlist[b][j] and Nlist[i][j] > Nlist[i][l]:
+                    topcount +=1
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+            if b == 0:
+                if Nlist[i][j] > Nlist[i][r] and Nlist[i][j] > Nlist[t][j] and Nlist[i][j] > Nlist[i][l]:
+                    topcount +=1
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+            if t > 0 and b > 0:
+                if Nlist[i][j] > Nlist[i][l] and Nlist[i][j] > Nlist[b][j] and Nlist[i][j] > Nlist[t][j]and Nlist[i][j]>Nlist[i][r]:
+                    print(i+1,"번째",Nlist[i][j],j+1,"칸")
+                    topcount +=1
+                
+print(topcount)
