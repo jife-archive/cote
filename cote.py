ee7f03ea17a8f8b9b202c0ae4c8b0022d9924988
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-06-29 18:07:43 +0900

    10814 나이순 정렬

--- cote.py
+++ cote.py
@@ -2,25 +2,12 @@ import sys
 
 N = int(sys.stdin.readline())
 Nlist = [[]for _ in range(N)]
-topcount = 0
-if N < 1 and N >50:
-    print("error")
-else:
-    for i in range(N):
-        Nlist[i] += list(map(int,(sys.stdin.readline().split())))
 
-x=[-1,0,1,0]
-y=[0,1,0,-1]
-Nlist.insert(0,[0]*N)
-Nlist.append([0]*N)
+for i in range(N):
+    age,name=  map(str,sys.stdin.readline().split()) 
+    Nlist[i] += int(age),name
+
+Nlist.sort(key=lambda x: x[0])
 
 for i in Nlist:
-    i.insert(0,0)
-    i.append(0)
-
-for i in range(1,N+1):
-    for j in range(1,N+1):
-        if all(Nlist[i][j]>Nlist[i+x[k]][j+y[k]]for k in range(4)):
-            topcount += 1
-            print(i,"번째",j,"칸",Nlist[i][j])
-print(topcount)
+    print(i[0],i[1])
