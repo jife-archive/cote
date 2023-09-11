df6c5c068164355223078d4567e2848499787ded
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-09-11 18:13:35 +0900

    28278번 스택2

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+let inputcount:Int = Int(readLine()!)!
+var stack: [Int] = []
 
+for _ in 0..<inputcount {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+
+    let command = input.first!
+    switch command {
+    case 1:
+        let result = input.last!
+        stack.append(result)
+    case 2:
+        print(stack.popLast() ?? -1)
+    case 3:
+        print(stack.count)
+    case 4:
+        print(stack.isEmpty ? 1: 0)
+    case 5:
+        print(stack.last ?? -1)
+    default:
+        fatalError()
+    }
+}
