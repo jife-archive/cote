13e2afbe56e9243dbdc2154291ad2dafb2443369
jife98@naver.com <114370871+jife98@users.noreply.github.com>
2023-09-12 15:32:14 +0900

    11279번 최대힙

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,32 +1,22 @@
-//
-//  main.swift
-//  Algorithm
-//
-//  Created by 최지철 on 2023/08/31.
-//
-
-import Foundation
-
-let inputcount:Int = Int(readLine()!)!
-var stack: [Int] = []
-
-for _ in 0..<inputcount {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-
-    let command = input.first!
-    switch command {
-    case 1:
-        let result = input.last!
-        stack.append(result)
-    case 2:
-        print(stack.popLast() ?? -1)
-    case 3:
-        print(stack.count)
-    case 4:
-        print(stack.isEmpty ? 1: 0)
-    case 5:
-        print(stack.last ?? -1)
-    default:
-        fatalError()
+let N = Int(readLine()!)!
+    var arr = [Int]()
+    
+    for _ in 0..<N {
+        let x = Int(readLine()!)!
+        if x == 0 {
+            if arr.count == 0 { print(0) }
+            else {
+                var max = 0, maxIndex = 0
+                for i in 0..<arr.count {
+                    if max < arr[i] {
+                        max = arr[i]
+                        maxIndex = i
+                    }
+                }
+                arr[maxIndex] = 0
+                print(max)
+            }
+        } else {
+            arr.append(x)
+        }
     }
-}
